1f81eba86102ccf3f5ad7400a7de1632296dd413
Varun <varun.v2021@gmail.com>
2018-11-05 12:14:54 +0530

    Update StreamAPIDemo.java

--- src/StreamAPIDemo.java
+++ src/StreamAPIDemo.java
@@ -6,10 +6,10 @@ import java.util.stream.*;
 /*
  * Stream In Java
 Introduced in Java 8, the Stream API is used to process collections of objects. A stream is a sequence of objects that supports various methods which can be pipelined to produce the desired result.
-The features of Java stream are –
+The features of Java stream are â€“
 
 A stream is not a data structure instead it takes input from the Collections, Arrays or I/O channels.
-Streams don’t change the original data structure, they only provide the result as per the pipelined methods.
+Streams donâ€™t change the original data structure, they only provide the result as per the pipelined methods.
 Each intermediate operation is lazily executed and returns a stream as a result, hence various intermediate operations can be pipelined. Terminal operations mark the end of the stream and return the result.
 Different Operations On Streams-
 Intermediate Operations:
@@ -71,7 +71,7 @@ public class StreamAPIDemo {
 
 		// demonstration of reduce method
 		int even = number.stream().filter(x -> x % 2 == 0).reduce(0, (ans, i) -> ans + i);
-
+		//int even = number.stream().filter(x -> x % 2 == 0).reduce(0, (ans, i) -> { System.out.println("ans: "+ans+ " i:"+i); return (ans + i);});
 		System.out.println(even);
 	}
 }
